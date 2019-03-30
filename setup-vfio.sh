020689b65f5484c9f5403b0f78e3e31a258d768a
da21510 <da21510@gmail.com>
2019-03-30 16:29:34 +0800

    bug-fix

--- setup-vfio.sh
+++ setup-vfio.sh
@@ -11,10 +11,10 @@ then
 	mv /etc/modprobe.d/vfio.conf /etc/modprobe.d/vfio.conf.bak
 fi
 
-for (( k = 1; k < 100; k++));
+for (( k = 1; k < 500; k++));
 do
 	a=$(echo $pci_id | cut -d' ' -f $i)
-	b=$(echo $vender_id | cut -d' ' -f $j)
+	b=$(echo $vender_id |cut -d' ' -f $j | cut -c1-7)
 
 	if [ "$a" == "$b" ];
 	then
@@ -22,9 +22,9 @@ do
 		then
 			echo "options vfio-pci ids=$(echo $vender_id | cut -d' ' -f $(echo "$j" + "2" | bc)) disable_vga=1" >> /etc/modprobe.d/vfio.conf
 			i=$(echo "$i" + "1"| bc)
-			j=$(echo "$j" + "5"| bc)
+			j=$(echo "$j" + "1"| bc)
 		fi
 	else
-		j=$(echo "$j" + "5"| bc)
+		j=$(echo "$j" + "1"| bc)
 	fi
 done
